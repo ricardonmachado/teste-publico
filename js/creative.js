282404bb76460971940620c2be40b63f271c5c42
Ricardo Machado <ricardonmachado@gmail.com>
2019-07-17 13:41:00 -0300

    teste site com o sagecell

--- js/creative.js
+++ js/creative.js
@@ -8,7 +8,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
+          scrollTop: (target.offset().top - 56)
         }, 1000, "easeInOutExpo");
         return false;
       }
@@ -23,15 +23,15 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 75
+    offset: 57
   });
 
   // Collapse Navbar
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-scrolled");
+      $("#mainNav").addClass("navbar-shrink");
     } else {
-      $("#mainNav").removeClass("navbar-scrolled");
+      $("#mainNav").removeClass("navbar-shrink");
     }
   };
   // Collapse now if page is not at top
@@ -39,8 +39,42 @@
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
 
+  // Scroll reveal calls
+  window.sr = ScrollReveal();
+
+  sr.reveal('.sr-icon-1', {
+    delay: 200,
+    scale: 0
+  });
+  sr.reveal('.sr-icon-2', {
+    delay: 400,
+    scale: 0
+  });
+  sr.reveal('.sr-icon-3', {
+    delay: 600,
+    scale: 0
+  });
+  sr.reveal('.sr-icon-4', {
+    delay: 800,
+    scale: 0
+  });
+  sr.reveal('.sr-button', {
+    delay: 200,
+    distance: '15px',
+    origin: 'bottom',
+    scale: 0.8
+  });
+  sr.reveal('.sr-contact-1', {
+    delay: 200,
+    scale: 0
+  });
+  sr.reveal('.sr-contact-2', {
+    delay: 400,
+    scale: 0
+  });
+
   // Magnific popup calls
-  $('#portfolio').magnificPopup({
+  $('.popup-gallery').magnificPopup({
     delegate: 'a',
     type: 'image',
     tLoading: 'Loading image #%curr%...',
